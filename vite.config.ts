b73656a3ca7b18314c0f21a88c3df0e778dad992
przemekszcz7 <przemekszcz7@gmail.com>
2026-05-19 13:35:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/smazalniaryba/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
